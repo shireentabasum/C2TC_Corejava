82c90a85028df50bfb68de56276d4c6116fe2e31
dell <dell@DESKTOP-A75L6PE>
2023-07-28 10:28:31 +0530

    first commit

---
+++ src/com/cg/dayone/Basics.java
@@ -0,0 +1,16 @@
+package com.cg.dayone;
+
+public class Basics {
+	
+	public static void main(String[] args) {
+		int a = 9;
+		int b = 2;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+	}
+
+}
